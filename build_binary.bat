4db8c7c1e69d9c81a4c52650e019dbec53ab7e76
s <ss>
2017-01-16 14:08:18 +0800

    1.加入编译后的assets目录

--- build_binary.bat
+++ build_binary.bat
@@ -1,5 +1,6 @@
 set path=%path%;E:\0000____OpenSourceProject\Go\bin;D:\worksoftware\Go\bin
 set GOROOT=D:\worksoftware\Go
 set GOPATH=%cd%
-go install -tags debug ngrok/main/ngrokd
+go install -tags release ngrok/main/ngrokd
+go install -tags release ngrok/main/ngrok
 pause
