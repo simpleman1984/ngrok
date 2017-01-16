7ec08347531cb5c4d0c6e91da7693a5401aee709
asd <12@12.xom>
2017-01-16 20:37:57 +0800

    ·

--- build_binary.bat
+++ build_binary.bat
@@ -1,5 +1,5 @@
 set path=%path%;E:\0000____OpenSourceProject\Go\bin;D:\worksoftware\Go\bin
-set GOROOT=D:\worksoftware\Go
+set GOROOT=E:\0000____OpenSourceProject\Go
 set GOPATH=%cd%
 go install -tags release ngrok/main/ngrokd
 go install -tags release ngrok/main/ngrok
